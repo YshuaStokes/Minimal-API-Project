b5cb9426fe2edd38b88aff5ad73e90115cb6754a
YshuaStokes <yshuastokes1@gmail.com>
2025-04-10 15:12:06 -0500

    commit

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,10 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('John', 'Doe'),
+		   ('Jane', 'Smith'),
+		   ('Alice', 'Johnson'),
+		   ('Bob', 'Brown'),
+		   ('Tim', 'Corey'),
+		   ('Yeshua', 'Stokes');
+end
---
+++ UserDB/dbo/StoredProcedures/spUser_Update.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Update]
+	@Id int,
+	@FirstName NCHAR(50),
+	@LastName NVARCHAR(50)
+AS
+begin
+	update dbo.[User]
+	set FirstName = @FirstName,
+		LastName = @LastName
+	where Id = @Id;
+end
